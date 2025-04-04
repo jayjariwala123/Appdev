7aeee81e19725f8a79a6c4c23fae0d6c1538aa8b
Jay Jariwala <jjariwala111@gmail.com>
2025-04-04 23:55:51 +0530

    Day 9 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,11 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
         fontFamily: GoogleFonts.lato().fontFamily,
       ),
-      //debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: "/",
+      initialRoute:  MyRoutes.homeRoute,
       routes: {
         "/" : (context) => LoginPage(),
         MyRoutes.homeRoute : (context) => HomePage(),
